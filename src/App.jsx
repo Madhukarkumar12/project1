b53704dbb9f0c0e3bdc3e26b99f623bb2f36ec33
Madhukar Kumar <150505032+Madhukarkumar12@users.noreply.github.com>
2026-02-17 11:40:40 +0530

    workflow

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import Login from "@/pages/Login";
 import { AuthProvider } from "@/context/AuthContext";
 import { useAuth } from "@/hooks/useAuth";
 import Dashboard from "./pages/Dashboard";
+// AppContent 컴포넌트를 별도로 만들어서 인증 상태와 로딩 상태를 처리하도록 분리
 
 const AppContent = () => {
   const { isAuthenticate, loading } = useAuth();
